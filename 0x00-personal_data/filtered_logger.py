b96ef40e6ccea5d8c502308eead398fde5fc8f0e
Brokeice123 <keithmarkomen@gmail.com>
2024-05-30 21:25:27 +0300

    Error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,7 +61,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connection.MySQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Function that returns a connector
     """
