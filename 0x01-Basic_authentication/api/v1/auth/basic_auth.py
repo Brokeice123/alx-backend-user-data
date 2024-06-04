6bc68d6cc9c7abd8b3013c646ae713144e665dee
Brokeice123 <keithmarkomen@gmail.com>
2024-06-04 15:18:27 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Python module for Basic Authentication
 """
 
 from base64 import b64decode
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -50,3 +52,24 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return decoded_base64_authorization_header.split(':', 1)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Returns user object based on email and password
+        """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            return User.search({'email': user_email})
+        except Exception:
+            return None
+        
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return None
