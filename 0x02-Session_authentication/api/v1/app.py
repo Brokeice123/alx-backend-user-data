7a922a2ca7397d1a6d1c1c39569e1bbc8798d471
Brokeice123 <keithmarkomen@gmail.com>
2024-06-06 07:14:58 +0300

    Error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,7 +36,8 @@ def request_filter() -> None:
                       '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
